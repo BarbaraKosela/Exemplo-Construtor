04feb8a25100c5763bd0d8d7db0071906162e526
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-15 16:54:11 -0300

    Passar informações

--- ExemploConstrutor/Model/Time.cs
+++ ExemploConstrutor/Model/Time.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    class Time
+    public class Time
     {
+        public string Nome { get; set; }
+        public string Pais { get; set; }
+        public int QuantidadeGols { get; set; }
+        public int QuantidadeGolsLevados { get; set; }
+        public int QuantidadeCartoesVermelhos { get;set; }
+        public int QuantidadeSemiFinais { get; set; }
+        public int QuantidadeVitorias { get; set; }
+        public decimal Faturamento { get; set; }
+        public int Substituicoes { get; set; }
+        public string Serie { get; set; }
+        public int QuantidadeDerrotas { get; set; }
+        public int QuantidadeGolsContras { get; set; }
+
+        public Time(string nome, string pais, string serie)
+        {
+            Nome = nome;
+            Pais = pais;
+            Serie = serie;
+        }
+
+
+
+        
     }
 }
---
+++ ExemploConstrutor/WFA/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.txtSerie = new System.Windows.Forms.TextBox();
+            this.txtFaturamento = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkGreen;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NOME:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkGreen;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PAÍS:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkGreen;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SÉRIE:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtNome.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtNome.Location = new System.Drawing.Point(81, 6);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(216, 26);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtPais
+            // 
+            this.txtPais.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtPais.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtPais.Location = new System.Drawing.Point(69, 36);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.Size = new System.Drawing.Size(228, 26);
+            this.txtPais.TabIndex = 4;
+            // 
+            // txtSerie
+            // 
+            this.txtSerie.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtSerie.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtSerie.Location = new System.Drawing.Point(78, 66);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(219, 26);
+            this.txtSerie.TabIndex = 5;
+            // 
+            // txtFaturamento
+            // 
+            this.txtFaturamento.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtFaturamento.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtFaturamento.Location = new System.Drawing.Point(463, 6);
+            this.txtFaturamento.Name = "txtFaturamento";
+            this.txtFaturamento.Size = new System.Drawing.Size(144, 26);
+            this.txtFaturamento.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.DarkGreen;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(322, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "FATURAMENTO:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Font = new System.Drawing.Font("Square721 BT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(326, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 52);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "SALVAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(627, 106);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFaturamento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSerie);
+            this.Controls.Add(this.txtPais);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Square721 BT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtPais;
+        private System.Windows.Forms.TextBox txtSerie;
+        private System.Windows.Forms.TextBox txtFaturamento;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
